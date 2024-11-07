a9ddff707ab50f4956b6d71af614084339851f59
yakkshit <63894727+yakkshit@users.noreply.github.com>
2024-11-08 02:24:16 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  domains: ["surviving-condor-cedz-dfdfcde1.koyeb.app"],
 };
 
 export default nextConfig;
